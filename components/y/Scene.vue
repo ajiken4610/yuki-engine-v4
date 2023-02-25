0c5e81cce596803bceb1f3081ddc6b47c2092681
ajiken4610 <ajiken4610@gmail.com>
2023-02-25 17:17:00 +0900

    YScene実装完了

---
+++ components/y/Scene.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+| YRenderer
+div
+  YObject3D(:object="scene" v-bind="props")
+    slot
+</template>
+<script setup lang="ts">
+import { Scene } from 'three';
+const props = withDefaults(defineProps<{
+  position?: Vec3,
+  rotation?: Vec3,
+  scale?: Vec3,
+  rotationOrder?: RotationOrder,
+}>(), {
+  position: () => ({ x: 0, y: 0, z: 0 }),
+  rotation: () => ({ x: 0, y: 0, z: 0 }),
+  scale: () => ({ x: 1, y: 1, z: 1 }),
+  rotationOrder: "XYZ"
+});
+
+const emit = defineEmits<(e: "value", val: Scene) => void>();
+
+// prepare scene
+const scene = new Scene()
+emit("value", scene)
+</script>
