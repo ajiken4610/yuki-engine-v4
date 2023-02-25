fd4d41872f1ff888bf3e4cd3bad4b0f77396a7cd
ajiken4610 <ajiken4610@gmail.com>
2023-02-25 11:42:19 +0900

    y-object-3d実装

--- components/y/Object3D.vue
+++ components/y/Object3D.vue
@@ -5,7 +5,30 @@ div
 </template>
 
 <script setup lang="ts">
-const props = withDefaults(defineProps<{
+import type { Object3D } from 'three';
 
-}>(), {});
+const props = withDefaults(defineProps<{
+  position: Vec3,
+  rotation: Vec3,
+  scale: Vec3,
+  rotationOrder: RotationOrder,
+  object: Object3D
+}>(), {
+  position: () => ({ x: 0, y: 0, z: 0 }),
+  rotation: () => ({ x: 0, y: 0, z: 0 }),
+  scale: () => ({ x: 1, y: 1, z: 1 }),
+  rotationOrder: "XYZ"
+});
+watch(() => props.position, (pos) => {
+  props.object.position.set(pos.x, pos.y, pos.z)
+}, { immediate: true })
+watch(() => props.rotation, (rot) => {
+  props.object.rotation.set(rot.x, rot.y, rot.z)
+}, { immediate: true })
+watch(() => props.scale, (scale) => {
+  props.object.scale.set(scale.x, scale.y, scale.z)
+}, { immediate: true })
+watch(() => props.rotationOrder, (order) => {
+  props.object.rotation.order = order;
+}, { immediate: true })
 </script>
--- pages/test/y-object3d.vue
+++ pages/test/y-object3d.vue
@@ -1,3 +1,8 @@
 <template lang="pug">
-YObject3D
+YObject3D(:object="object")
 </template>
+<script setup lang="ts">
+import { Object3D } from 'three';
+
+const object = new Object3D();
+</script>
