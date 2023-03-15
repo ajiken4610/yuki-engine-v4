0c1739b0343a9c77df5855a15ad58a37d6d42fec
ajiken4610 <github>
2023-03-15 19:06:05 +0900

    YMaterialStandard実装

---
+++ components/y/material/Standard.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+| YMaterialStandard
+div
+  YMaterial
+</template>
+
+<script setup lang="ts">
+import { MeshStandardMaterial } from 'three';
+
+const emit = defineEmits<(e: "value", value: MeshStandardMaterial) => void>();
+const material = new MeshStandardMaterial();
+emit("value", material)
+onUnmounted(() => {
+  material.dispose()
+})
+</script>
---
+++ pages/test/y-material-standard.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+div
+  YMaterialStandard(@value="material = $event")
+</template>
+
+<script setup lang="ts">
+import type { MeshStandardMaterial } from 'three';
+
+let material: MeshStandardMaterial;
+onMounted(() => {
+  console.log(material)
+})
+</script>
