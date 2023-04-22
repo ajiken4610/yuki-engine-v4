70a691ce5f1ae333386e4033bed802b3e959c1ac
ajiken4610 <github>
2023-04-23 07:56:16 +0900

    YMaterialBasicの実装

---
+++ components/y/material/Basic.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+| YMaterialBasic
+div
+  YMaterial
+</template>
+
+<script setup lang="ts">
+import { MeshBasicMaterial } from 'three';
+
+const emit = defineEmits<(e: "value", value: MeshBasicMaterial) => void>();
+const material = new MeshBasicMaterial();
+emit("value", material)
+onUnmounted(() => {
+  material.dispose()
+})
+</script>
---
+++ pages/test/y-material-basic.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+div
+  YMaterialBasic(@value="material = $event")
+</template>
+
+<script setup lang="ts">
+import type { MeshBasicMaterial } from 'three';
+
+let material: MeshBasicMaterial;
+onMounted(() => {
+  console.log(material)
+})
+</script>
