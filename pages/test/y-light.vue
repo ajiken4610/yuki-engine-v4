c39ceab461a95a8bbcffd5b279b4deb596fda2d3
ajiken4610 <github>
2023-04-30 08:16:21 +0900

    ylight実装

---
+++ pages/test/y-light.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+YScene(@value="scene=$event" v-slot="{p}")
+  YLight(:object="light" :p="p")
+</template>
+
+<script setup lang="ts">
+import { Light, type Scene } from 'three';
+const light = new Light(0xff0000)
+let scene: Scene
+onMounted(() => {
+  console.log(scene)
+})
+</script>
